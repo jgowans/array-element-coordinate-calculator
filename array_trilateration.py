1805981e4199fc442261935dc4f79a160e48eb69
James Gowans <gowans.james@gmail.com>
2015-11-27 12:49:51 +0200

    wrote matrix builder and user input getter

--- array_trilateration.py
+++ array_trilateration.py
@@ -1,13 +1,62 @@
 #!/usr/bin/env python
 
 import argparse
-import numpy
+import numpy as np
+import itertools
 
-def run(n, *args):
+def get_measurements_from_user(n):
+    """ n: number of elements. 
+    Prompts for input and returns a hash like: 
+    {'d01': 1.23, 'd02': 2.34, ..., 'd34': 5.76}
+    """
+    distances = {}
+    pairs = itertools.combinations(range(0, n), 2)
+    for pair in pairs:
+        el0 = pair[0]
+        el1 = pair[1]
+        while True:    # keep trying to get valid input
+            try:
+                d = raw_input("Distance from elelemt {el0} to element {el1}:  ".format(el0 = el0, el1 = el1))
+                d = np.float64(d)   # here is where an exception could be thrown
+                assert((d > 0) or (d == -1))
+                break
+            except (TypeError, AssertionError):
+                print("Invalid input! Enter -1 for no reading taken. Re-try.")
+        key = 'd{n}{m}'.format(n = el0, m = el1)
+        distances[key] = d
+    print distances
+    return distances
+
+def initial_trilateration(p0, p1, distance_matrix):
+    for target_number in range(2, distance_matrix.shape[0]):
+        d0t = distance_matrix[0][target_number]
+        c0t = Circle(p0, d0t)
+        d1t = distance_matrix[1][target_number]
+        c1t = Circle(p1, d1t)
+
+def build_distance_matrix(n, **kwargs):
+    """ n = number of elements.
+    kwargs like: {'d01': 1.23, 'd02': 2.34, ..., 'd34': 5.76}
+    val should be -1 if measurement not taken
+    """
+    distance_matrix = np.zeros((n, n))
+    pairs = itertools.combinations(range(0, n), 2)
+    for pair in pairs:
+        el0 = pair[0]
+        el1 = pair[1]
+        key = 'd{n}{m}'.format(n = el0, m = el1)
+        distance = kwargs[key]
+        distance_matrix[el0][el1] = distance
+        distance_matrix[el1][el0] = distance
+    return distance_matrix
+
+def run(n, **kwargs):
+    distance_matrix = np.zeros((n, n))
+    points = []
     return []
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "Calculate coordinates from side lengths")
-    parser.add_argument('-n', "Number of array elements")
-    args.parser.parse_args()
-    leng
+    parser.add_argument('-n', default = 4, type = int, help = "Number of array elements")
+    args = parser.parse_args()
+    get_measurements_from_user(args.n)
--- tests/test_array_trilateration.py
+++ tests/test_array_trilateration.py
@@ -12,31 +12,49 @@ from point import Point
 # 3 @ (1, 5)
 
 class ArrayTrilateration(unittest.TestCase):
-    def test_four_element(self):
-        p0 = Point(0, 0)
-        p1 = Point(4, 0)
-        p2 = Point(3, 4)
-        p3 = Point(1, 5)
-        d01 = p0.distance_to_other(p1)
-        d02 = p0.distance_to_other(p2)
-        d03 = p0.distance_to_other(p3)
-        d12 = p1.distance_to_other(p2)
-        d13 = p1.distance_to_other(p3)
-        d23 = p2.distance_to_other(p3)
-        print("0->1: {d}".format(d = d01))
-        print("0->2: {d}".format(d = d02))
-        print("0->3: {d}".format(d = d03))
-        print("1->2: {d}".format(d = d12))
-        print("1->3: {d}".format(d = d13))
-        print("2->3: {d}".format(d = d23))
+    def setUp(self):
+        self.p0 = Point(0, 0)
+        self.p1 = Point(4, 0)
+        self.p2 = Point(3, 4)
+        self.p3 = Point(1, 5)
+        self.d01 = self.p0.distance_to_other(self.p1)
+        self.d02 = self.p0.distance_to_other(self.p2)
+        self.d03 = self.p0.distance_to_other(self.p3)
+        self.d12 = self.p1.distance_to_other(self.p2)
+        self.d13 = self.p1.distance_to_other(self.p3)
+        self.d23 = self.p2.distance_to_other(self.p3)
+        print("0->1: {d}".format(d = self.d01))
+        print("0->2: {d}".format(d = self.d02))
+        print("0->3: {d}".format(d = self.d03))
+        print("1->2: {d}".format(d = self.d12))
+        print("1->3: {d}".format(d = self.d13))
+        print("2->3: {d}".format(d = self.d23))
+
+    def test_build_array(self):
+        expected = np.array([
+            [       0, self.d01, self.d02, self.d03], 
+            [self.d01,        0, self.d12, self.d13],
+            [self.d02, self.d12,        0, self.d23],
+            [self.d03, self.d13, self.d23,        0]
+        ])
+        distances = {'d01':self.d01, 
+                     'd02':self.d02, 
+                     'd03':self.d03, 
+                     'd12':self.d12, 
+                     'd13':self.d13, 
+                     'd23':self.d23}
+        output = array_trilateration.build_distance_matrix(4, **distances)
+        self.assertEqual(output.shape, expected.shape)
+        self.assertTrue((output == expected).all())
+
+    def skip_test_four_element(self):
         array = np.array([
             [  0, d01, d02, d03], 
             [d01,   0, d12, d13],
             [d02, d12,   0, d23],
             [d03, d13, d23,   0]
         ])
-
-        result = array_trilateration.run(4, d01, d02, d03, d12, d13, d23)
+        result = array_trilateration.run(4, distances)
         self.assertEqual(len(result), 4)
         self.assertIn(p0, result)
         self.assertIn(p1, result)
